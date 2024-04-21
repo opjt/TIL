fc574337b59714e6dff17210ee0bea456849284f
opjt <opjt@users.noreply.github.com>
2024-04-21 08:02:55 +0000

    deploy: 915a16c455822119b165156c03b4393925d96fe0

---
+++ assets/foo.html-BN7Dszjq.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o}from"./app-GLJH42Vd.js";const a={};function r(c,n){return t(),o("div")}const s=e(a,[["render",r],["__file","foo.html.vue"]]),_=JSON.parse('{"path":"/group/foo.html","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1713686543000},"filePathRelative":"group/foo.md"}');export{s as comp,_ as data};

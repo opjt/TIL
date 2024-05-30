29c1fa8206a2e83da1922bec0a30bb2056154dc1
opjt <jtpark1957@gmail.com>
2024-05-30 09:33:23 +0900

    [post] bj1931

--- ETC/main.py
+++ ETC/main.py
@@ -1,13 +1,43 @@
-count, num1 = map(int, input().split())
-numbers = []
+N = int(input())
 
-for i in range(count):
-   numbers.append(int(input()))
+meetings = []
 
-# print("max : " + str(max))
+for _ in range(N):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
 
-totalCount = 0
-for i in reversed(range(count)):
-    totalCount += num1//numbers[i]
-    num1 = num1 % numbers[i]
-print(totalCount)
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 0
+last_end_time = 0
+
+for x, y in meetings:
+    print(str(x) + " " + str(y))
+
+for start, end in meetings:
+    if start >= last_end_time:
+        count += 1
+        last_end_time = end
+
+print(count)
+
+# for i in range(len(lng)):
+#     print(lng[i])
+
+
+# (3,5) (1,2) (4,7) (5,8)
+
+
+# count, num1 = map(int, input().split())
+# numbers = []
+
+# for i in range(count):
+#    numbers.append(int(input()))
+
+# # print("max : " + str(max))
+
+# totalCount = 0
+# for i in reversed(range(count)):
+#     totalCount += num1//numbers[i]
+#     num1 = num1 % numbers[i]
+# print(totalCount)

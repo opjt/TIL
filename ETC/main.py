5bcf125e7aa77452f487b2750e941be6fcb5a148
opjt <jtpark1957@gmail.com>
2024-08-29 23:31:00 +0900

    daily commit

--- ETC/main.py
+++ ETC/main.py
@@ -1,39 +1,9 @@
-def solution(X, Y):
-    answer = []
-    for i in (set(X) & set(Y)):
-        for j in  range(min(X.count(i), Y.count(i))):
-            answer.append(i)
-    answer.sort(reverse=True)
-    if len(answer) == 0:
-        return '-1'
-    if answer[0] == '0':
-        return '0'
-    return ''.join(answer)
-
-
-
-
+def solution(numbers):
+	# 3번씩 이어 붙인 문자열들을 내림차순으로 정렬
+    numbers = sorted(numbers, key=lambda x: str(x)*3, reverse=True)
     
-print(solution("100","203045"))
-
-
-# def solution(X, Y):
-#     answer = ""
-#     listY = list(Y)
-#     for i, val in enumerate(X):
-#         if val in listY:
-#             position = listY.index(val)
-#             if(position != -1):
-#                 if(answer == ""):
-#                     answer = listY[position]
-#                 elif(answer[0] == "0" and listY[position] == "0"):
-#                     answer = "0"
-#                 elif(answer[0] <= listY[position]):
-#                     answer = listY[position]+answer
-#                 elif(answer[0] > listY[position]):
-#                     answer = answer + listY[position]
-               
-#                 listY[position] = "."
-#     if(answer == ""):
-#         return "-1"
-#     return answer
+    #문자열들을 전부 이어 붙인다.
+    answer = ''.join(map(str, numbers))
+    
+    # numbers의 원소가 모두 0일 때
+    return answer if int(answer) != 0 else '0'

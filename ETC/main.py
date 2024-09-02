7b908510b3c0272457e9f13b89d332bef2d792b4
opjt <jtpark1957@gmail.com>
2024-09-02 22:47:03 +0900

    daily commit

--- ETC/main.py
+++ ETC/main.py
@@ -1,10 +1,12 @@
-def solution(numbers):
-	dict = {}
-	for i in numbers:
-		dict[i] = True
-	if(len(numbers)/2 < len(dict)):
-		return int(len(numbers)/2)
-	return len(dict)
-		
+def mini(array, i, j, k):
+    newarray = array[i-1:j]  # 인덱스 슬라이싱 수정 (j번째는 포함해야 하므로 j-1 대신 j)
+    return sorted(newarray)[k-1]  # 슬라이싱한 배열을 정렬 후 k번째 값 반환
 
-print(solution([3,3,3,2,2,2]))
+def solution(numbers, command):
+    result = []
+    for cmd in command:
+        i, j, k = cmd
+        result.append(mini(numbers, i, j, k))
+    return(result)
+
+
